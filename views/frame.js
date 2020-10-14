91ed5048edff904d5a4e970a08b3fc15a7142d3b
Christian Engel <chrispy@parastudios.de>
2020-10-14 15:06:49 +0200

    Add viewport

--- views/frame.js
+++ views/frame.js
@@ -3,6 +3,7 @@ const frame = (site, title, description, body) => `<!DOCTYPE html>
     <head>
         <link href="../assets/favicon.svg" rel="shortcut icon">
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
         <title>${title} - ${site.title}</title>
         ${description ? `<meta name="description" content="${description}" />` : ""}
         <link type="text/css" rel="stylesheet" href="../assets/style.css" />
